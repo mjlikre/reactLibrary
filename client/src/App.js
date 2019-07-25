ff7ce688cbbfaa44b235d479e3d27ea6b1e2bc10
Michael's <mjlikre@gmail.com>
2019-07-25 13:02:52 -0700

    aop

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path = '/' compoment = {Books}/>
+          <Route exact path = '/books' compoment = {Books}/>
+          <Route exact path = '/books/:id' component = {Detail}/>
+          <Route compoment = {NoMatch}/>
+
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
